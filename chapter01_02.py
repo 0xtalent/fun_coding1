22ba5a9df34ed1b1428065746e23ecc9c6561e5f
marinehayong <marinehayong@gmail.com>
2019-12-11 21:35:19 +0900

    크롤링을 위한 지식: 웹 구조와 HTML 이해하기

---
+++ chapter01_02.py
@@ -0,0 +1,11 @@
+# Chapter 01 크롤링과 웹 기본
+# chapter 01-2 크롤링을 위한 지식: 웹 구조와 HTML 이해하기 1
+# 2019-12-11 21:09
+
+import requests
+from bs4 import BeautifulSoup
+import lxml
+
+'''
+HTML은 마크업 언어(문서와 데이터의 구조를 표현하는 언어)입니다.
+'''
